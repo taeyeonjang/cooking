2999e55e0fa833c150308e8c9cef9f4d7484c029
tyJang <wxd8339@gmail.com>
2022-04-28 19:51:23 +0900

    start

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <>
+            <div className="app">
+                <span style={{ fontSize: '2rem' }}>오늘 뭐 묵지?</span>
+            </div>
+
+        </>
+    )
+}
+
+export default LandingPage
